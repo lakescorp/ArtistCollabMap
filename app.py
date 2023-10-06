fa4e3cb6fa6d14d5a2974910fedcbd01eeddf4b2
lakescorp <lakescorporation@gmail.com>
2023-10-06 19:03:26 +0200

    🖼️ Images upscaling on hover

--- app.py
+++ app.py
@@ -17,17 +17,16 @@ artist_data_store = {
 }
 
 dash_app.layout = html.Div(style={"display": "flex", "height": "100%", "flexDirection": "column"}, children=[
-    # Formulario en la parte superior
     html.Div(style={"display": "flex", "justifyContent": "space-between", "alignItems": "center", "marginBottom": "20px"}, children=[
-        # Formulario
+
         html.Div(children=[
             html.Label("Artist:", htmlFor="artist_link"),
             dcc.Input(type="text", id="artist_link"),
             html.Button("Generate graph", id="generate-button", style={
-                "backgroundColor": "#1DB954",  # Verde Spotify
+                "backgroundColor": "#1DB954", 
                 "border": "none",
-                "borderRadius": "25px",  # Bordes completamente redondeados
-                "color": "white"  # Texto blanco
+                "borderRadius": "25px", 
+                "color": "white" 
             })
         ]),
         
@@ -37,9 +36,8 @@ dash_app.layout = html.Div(style={"display": "flex", "height": "100%", "flexDire
 
     ]),
     
-    # Contenedor principal para gráfico y galería
+
     html.Div(style={"display": "flex", "height": "100%", "flexDirection": "row", "overflow": "hidden"}, children=[
-        # Contenedor para el gráfico
         html.Div(id='graph-container', style={"width": "50%", "paddingBottom": "50%", "position": "relative", "flexShrink": 0}, children=[
             dcc.Graph(id='artist-network', style={
                 "position": "absolute",
@@ -50,13 +48,14 @@ dash_app.layout = html.Div(style={"display": "flex", "height": "100%", "flexDire
             }),
         ]),
 
-        # Galería de canciones a la derecha
         html.Div(id='click-data', style={
             "flex": "1",
             "overflowY": "auto",
             "maxHeight": "500px",
             "width": "50%",
-            "paddingLeft": "20px"
+            "paddingLeft": "20px",
+            "color": "inherit",  
+            "textDecoration": "none"  
         }, children=[
             html.Button('Generate graph for artist', id='gen-selec-artist', n_clicks=0, style={'display': 'none'}),
             html.Div(id='songs-list', children='Click on a node to see more details')
@@ -114,10 +113,10 @@ def display_click_data(clickData):
 
     generate_button_for_artist = html.Button('Generate graph for artist', id='gen-selec-artist', n_clicks=0, **{'data-artist-id': node_clicked_id})
     generate_button_for_artist.style = {
-            "backgroundColor": "#1DB954",  # Verde Spotify
+            "backgroundColor": "#1DB954", 
             "border": "none",
-            "borderRadius": "25px",  # Bordes completamente redondeados
-            "color": "white",  # Texto blanco
+            "borderRadius": "25px", 
+            "color": "white", 
             "marginLeft": "auto"
         }
     
@@ -137,7 +136,7 @@ def display_click_data(clickData):
         song_info = artist_data_store["registered_songs"][song_id]
         song_title = html.Div(song_info['name'], style={"textAlign": "center"})
 
-        audio_preview = html.Audio(src=song_info['preview'], id=f"audio-{song_id}", controls=False)  # Sin controles
+        audio_preview = html.Audio(src=song_info['preview'], id=f"audio-{song_id}", controls=False) 
 
         song_thumbnail = html.Img(src=song_info['thumbnail'], 
                              className="song-thumbnail", 
@@ -145,9 +144,12 @@ def display_click_data(clickData):
                              **{'data-song-id': song_id})
 
 
-        song_item = html.Div([song_thumbnail, song_title, audio_preview], style={"display": "inline-block", "margin": "10px"})
+        song_item = html.Div([song_thumbnail, song_title, audio_preview], 
+                     className="gallery-item-div", 
+                     style={"display": "inline-block", "margin": "10px"})
 
-        gallery_item = html.A(song_item, href=song_info['url'], target="_blank")
+
+        gallery_item = html.A(song_item, href=song_info['url'], target="_blank", className="gallery-item")
         gallery_items.append(gallery_item)
 
     return html.Div([artist_name] + gallery_items), node_clicked_id
@@ -168,30 +170,25 @@ def display_click_data(clickData):
     ]
 )
 def unified_update_graph(n_clicks_generate, n_clicks_selected, input_artist, artist_id):
-    # Determinar qué botón se presionó
     ctx = dash.callback_context
 
-    # Si no se presionó ninguno de los botones
     if not ctx.triggered:
         raise PreventUpdate
 
     if ctx.triggered[0]['prop_id'] == "gen-selec-artist.n_clicks" and n_clicks_selected == 0:
         raise PreventUpdate
 
-
-    print(f"Callback 'unified_update_graph' fue disparada por {ctx.triggered[0]['prop_id']}")
-    print(n_clicks_generate,n_clicks_selected)
     button_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
     if button_id == 'generate-button':
         if not input_artist:
             raise PreventUpdate
-        # Aquí, usa el input_artist como antes
+
         total_artists, registered_songs, last_artist_collab, artist_data, artist_info, artist_songs = process_artist_collabs(input_artist)
     elif button_id == 'gen-selec-artist':
         if not artist_id:
             raise PreventUpdate
-        # Usa artist_id directamente
+
         total_artists, registered_songs, last_artist_collab, artist_data, artist_info, artist_songs = process_artist_collabs(artist_id)
 
     artist_data_store["songs"] = artist_songs
@@ -203,7 +200,7 @@ def unified_update_graph(n_clicks_generate, n_clicks_selected, input_artist, art
     artist_name = artist_data_store["artist_info"].get(artist_data["id"], {}).get('name', None) if artist_data else None
     artist_image = artist_data_store["artist_info"].get(artist_data["id"], {}).get('url', None) if artist_data else None
 
-    # Crear detalles del artista para mostrar en el layout
+
     artist_details = []
     if artist_name and artist_image:
         artist_details.append(html.Div(artist_name, style={"marginRight": "10px"}))
